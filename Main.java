a88dd79a1b91ee820c357ddf985f289697c5b822
Seventhy-Seven <72259094+Seventhy-Seven@users.noreply.github.com>
2020-10-02 21:34:29 +0500

    Add files via upload
    Напишите программу, которая выводит последовательность чисел
    Фибоначчи. Первые два числа в этой последовательности равны 1, а каждое
    следующее число равно сумме двух предыдущих (получается последовательность 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 и так далее). Количество чисел
    в последовательности вводится пользователем. Предложите версии
    программы, использующие разные операторы цикла.

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+
+class project3 {
+    public static void main(String[] args) {
+
+        System.out.print("Введите число n: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] f = new int[n];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+
+        for (int i = 0; i < n; ++i) {
+            System.out.println(f[i]);
+        }
+
+    }
+}
